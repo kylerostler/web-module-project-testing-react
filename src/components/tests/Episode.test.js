1cda7c745e4f72b734a3dc0509c20189a44da033
kylerostler <93017344+kylerostler@users.noreply.github.com>
2022-02-21 17:59:29 -0700

    finished episode tests

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,8 +3,28 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+const testingProps = {
+    id: 1,
+    image: 'img here',
+    name: 'name',
+    season: 1,
+    number: 1,
+    summary: 'summary',
+    runtime: 1
+}
 
-test("renders the summary test passed as prop", () => { });
+test("renders without error", () => {
+    render(<Episode episode={testingProps}/>)
+ });
 
-test("renders default image when image is not defined", () => { });
+test("renders the summary test passed as prop", () => { 
+    render(<Episode episode={testingProps} />)
+    const summary = screen.queryByText('summary')
+    expect(summary).toBeInTheDocument()
+});
+
+test("renders default image when image is not defined", () => {
+    render(<Episode episode={testingProps} />)
+    const defaultImage = screen.queryAllByAltText('./stranger_things.png')
+    expect(defaultImage).toBeInTheDocument()
+ });
